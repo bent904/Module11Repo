afd909aa6a0295cb152048bf74f2edf48f8b1d43
bent904 <cbentley_2@hotmail.com>
2026-04-19 21:40:49 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
-# Your Name
-# Date
+# Christopher Bentley
+# 4/19/2026
 # A simple Python program to print a message
 
+# Christopher Bentley
+# 4/19/2026
+# added new message
+
 print("Hello, World!")
+print("Welcome to GitHub!")
